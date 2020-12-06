9aebc63ea6f25d71cf94e6e17c7583ed161fac44
Oleg <>
2020-12-06 15:05:46 +0300

    Added

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let url = window.location.toString();
-  const getName = (url) => {
-    let urlIn = url.split('=');
-    let name = urlIn[1];
-    if (name == undefined) {name = 'StrigunovOleg';
+let url0 = window.location.toString();
+  const getName0 = (url0) => {
+    let urlIn = url0.split('=');
+    let name0 = urlIn[1];
+    if (name0 == undefined) {name0 = 'StrigunovOleg';
   }
-return name;
+return name0;
 }
 
+const name = `${getName0(url0)}`;
+const url = 'https://api.github.com/users/';
+const log = console.log;
 
-fetch(`https://api.github.com/users/${getName(url)}`)
+const getName = new Promise((resolve, reject) => {
+  setTimeout(() => name?resolve(name):reject('Не найдено имя'), 3000);
+})
+
+const getUrl = new Promise((resolve, reject) => {
+  setTimeout(() => url?resolve(url):reject('Не найдено ссылка'), 2000);
+})
+
+Promise.all([getName, getUrl])
+  .then(([name, url]) => fetch(`${url}${name}`))
   .then(res => res.json())
   .then(json => {
     const img = document.createElement('img');
@@ -28,8 +40,9 @@ fetch(`https://api.github.com/users/${getName(url)}`)
     url.href = json.html_url;
     url.innerHTML = 'Открыть профиль';
     document.body.append(url);
+
+    const d = document.createElement('p');
+    d.innerHTML = Date();
+    document.body.append(d);
   })
-
-  .then(json => console.log(json.avatar_url.reverse().join('')))
-
-  .catch(err => console.log(err))
+  .catch(err => log(err));
